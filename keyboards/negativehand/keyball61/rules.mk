f51ae4d090ff26cea0caa892236341ed7c2ad083
NegativeHand <34948783+NegativeHand@users.noreply.github.com>
2025-05-08 21:11:31 -0400

    enabled OLED

--- keyboards/negativehand/keyball61/rules.mk
+++ keyboards/negativehand/keyball61/rules.mk
@@ -32,7 +32,7 @@ RGB_MATRIX_DRIVER = ws2812
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 # To support OLED
-OLED_ENABLE = no                # Please Enable this in each keymaps.
+OLED_ENABLE = yes                # Please Enable this in each keymaps.
 SRC += lib/oledkit/oledkit.c    # OLED utility for Keyball series.
 
 # Include common library
